e316d73b98e48efe6d998938c57a24d95c18cce6
Yosi Izhak <yosii84@gmaail.com>
2021-03-17 22:54:02 +0200

    aaaaa

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -2,32 +2,21 @@
 import React, { Component } from 'react'
 import './App.css';
 
-import CheckBox from './components/check11.2/check11.2'
+import Form from './components/form11.3/form11.3'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      terms: [
-        { id: 1, value: "i read", checked: false },
-        { id: 2, value: "i accept", checked: false },
-        { id: 3, value: "yes", checked: true },
-        { id: 4, value: "no", checked: true }
-      ]
+   
      
     }
-  }
+  
 
   
 
   render() {
     return (
       <div>
-        {
-          this.state.terms.map((p) => {
-            return <CheckBox key={p.id} value={p.value} checked={p.checked} />
-          })
-        }
-
+             <Form  />
       </div>
     );
   }
--- myreact/src/components/form11.3/form11.3.jsx
+++ myreact/src/components/form11.3/form11.3.jsx
@@ -1,21 +1,60 @@
 import React from 'react'
-import './child11.1.css'
+//import './child11.1.css'
 
 
 class Form extends React.Component {
 
     constructor(props) {
         super(props);
-      
+        this.state = {
+           firstName:"",lastName:"",area:"",select:""   };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+    handleInputChange(e){ 
+        this.setState({ 
+          [e.target.name] : e.target.value 
+        }) 
+      } 
+      handleSubmit(e){ 
+        const { name,last,  area, select } = this.state 
+        e.preventDefault() 
+        window.confirm(` 
+          ____Your Details____\n 
+          firstName : ${name} 
+          lastName : ${last} 
+        
+          area : ${area} 
+          select : ${select} 
+        `) 
+      } 
 
     render() {
         return (
-
-            <div>
-          
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    firstName:
+              <input type="text" value={this.state.value}name="first" onChange={this.handleInputChange} />
+                </label><br />
+                <label>
+                    lastName:
+              <input type="text" value={this.state.value}name="last" onChange={this.handleInputChange} />
+                </label>
+                <label><br /><br />
+          textArea:
+          <textarea value={this.state.value}name="area" onChange={this.handleInputChange} />
+                </label><br /><br />
+                <label>
+                    Pick your choose:
+          <select value={this.state.value}name="select" onChange={this.handleInputChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         )
     }
 }
