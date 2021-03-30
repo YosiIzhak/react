8df961c7517df2d7b25fa9f5cc5eee78325c74de
Yosi Izhak <yosii84@gmail.com>
2021-03-30 17:43:52 +0300

    First commit

--- myreact/src/components/noris19.4/noris19.4.jsx
+++ myreact/src/components/noris19.4/noris19.4.jsx
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Api  (){
-    const [categories, setCetegories]=useState('');
-    const [term, setTerm]=useState('random');
-    const [joke, setJoke]=useState('');
-   
-           
- 
-            useEffect(() => {
-        const req = async ()=>{
-           const response = await axios.get('https://api.chucknorris.io/jokes/categories')
-           setCetegories(response.data )
+function Api() {
+    const [categories, setCetegories] = useState('');
+    const [term, setTerm] = useState('random');
+    const [joke, setJoke] = useState('');
+
+
+
+    useEffect(() => {
+        const req = async () => {
+            const response = await axios.get('https://api.chucknorris.io/jokes/categories')
+            setCetegories(response.data)
         }
         req();
     }, []);
-       
 
-    }
-     const categorySelect = (e) => {
+
+
+    const categorySelect = (e) => {
         setTerm(e.target.value)
     }
-     const getJoke = async () => {
+   
+    const getJoke = async () => {
         let cat;
         if (term === 'random') {
             cat = await axios.get('https://api.chucknorris.io/jokes/random')
@@ -32,19 +33,19 @@ function Api  (){
         setJoke(cat.data.value)
     }
 
-        return (
-            <div>
-                <button onClick={getJoke}>click me</button>
-                <select name="" id="" onChange={categorySelect}>
-                    {
-                        categories.map((x) => {
-                            return <option value={x} key={x}>{x}</option>
-                        })
-                    }
-                </select>
-                <p>{joke}</p>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <button onClick={getJoke}>click me</button>
+            <select name="" id="" onChange={categorySelect}>
+                {
+                    categories.map((x) => {
+                        return <option value={x} key={x}>{x}</option>
+                    })
+                }
+            </select>
+            <p>{joke}</p>
+        </div>
+    )
+
 }
 export default Api
