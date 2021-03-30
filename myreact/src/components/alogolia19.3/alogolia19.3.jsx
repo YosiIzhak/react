e1edf77e5000b0297185bbe17fdbed67c6eef8ea
Yosi Izhak <yosii84@gmail.com>
2021-03-30 22:21:19 +0300

    First commit

--- myreact/src/components/alogolia19.3/alogolia19.3.jsx
+++ myreact/src/components/alogolia19.3/alogolia19.3.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from 'react-loader-spinner';
+import './alogolia19.3.css'
 function Alogo() {
     const [result, setResult] = useState([]);
     const [search, setSearch] = useState('hooks')
     const [submit, setSubmit] = useState('hooks')
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const req = async () => {
             try {
                 const response = await axios.get(`https://hn.algolia.com/api/v1/search?query=${search}`)
                 setResult(response.data.hits)
+                setLoading(false)
             } catch (e) {
                 console.log('error')
             }
@@ -25,20 +30,26 @@ function Alogo() {
         setSearch(submit)
 
     }
-
-
-    return (
-        <div className="app">
-            <label>Search</label>
-            <input type={'text'} value={submit} onChange={(e)=>{mySearch(e)}} />
-            <input type={'button'} value='click' onClick={GetSubmit} />
-            {
-             result.map((x,index) => {
-            return <h5 key={index}>{x.title}</h5>
-        })
-    }
-
+    if (loading) {
+        return(
+        <div className="load" >
+        <Loader type="Circles" color="#00BFFF" height={180} width={180} />
         </div>
-    );
+        )}
+    else {
+        return (
+            <div className="app" >
+                <label>Search</label>
+                <input type={'text'} value={submit} onChange={(e) => { mySearch(e) }} />
+                <input type={'button'} value='click' onClick={GetSubmit} />
+                {
+                    result.map((x, index) => {
+                        return <p key={index}><a href={x.url} target="_blank">{x.title}</a></p>
+                    })
+                }
+
+            </div >
+        )
+    }
 }
 export default Alogo;
